a3b8a345e01edaf74b674e868fa01440755de983
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2021-02-07 20:04:20 +0530

    adding player movement

--- game.js
+++ game.js
@@ -2,6 +2,12 @@ function loadImages() {
     //player, virus, gem
     enemyImage = new Image();
     enemyImage.src = 'Assets/v1.png';
+
+    playerImage = new Image();
+    playerImage.src = 'Assets/superhero.png';
+
+    gemImage = new Image();
+    gemImage.src = 'Assets/gemm.png';
 }
 function init() {
     //define the objects that we will have in the game
@@ -37,29 +43,54 @@ function init() {
     };
 
     enemies = [enemy1, enemy2, enemy3];
+
+    player = {
+        x: 20,
+        y: H/2,
+        w: 60,
+        h: 60,
+        speed: 20,
+        moving: 'false',
+    }
+    gem = {
+        x: W-100,
+        y: H/2,
+        w: 60,
+        h: 60,
+    }
+
+    //listen to events  on the canvas
+    document.addEventListener('keydown', (e) => {
+        if(e.key == 'ArrowRight') {
+            player.moving = true;
+        }
+    });
+    document.addEventListener('keyup', () => {
+        player.moving = false;
+    });
 }
 function draw() {
     //clear the canvas area for the old frame
     pen.clearRect(0, 0, W, H);
+
+    pen.drawImage(playerImage, player.x, player.y, player.w, player.h);
+    pen.drawImage(gemImage, gem.x, gem.y, gem.w, gem.h);
 
     enemies.forEach(enemy => {
         pen.drawImage(enemyImage, enemy.x, enemy.y, enemy.w, enemy.h); 
     });
 }
 function update() {
+    if(player.moving == true) {
+        player.x += player.speed;
+    }
+
     enemies.forEach(enemy => {
         enemy.y += enemy.speed;
         if(enemy.y > H - enemy.h || enemy.y < 0) {
             enemy.speed *= -1;
         }
     })
-    
-    //move the box downwards
-    box.y += box.speed;
-
-    if(box.y > H - box.h || box.y < 0) {
-        box.speed *= -1;
-    }
 }
 function gameloop() {
     draw();
