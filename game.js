a8a58c978045136e2d148a7883c21fe0001ff4ad
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2021-02-07 19:44:56 +0530

    building enemies

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 function loadImages() {
     //player, virus, gem
+    enemyImage = new Image();
+    enemyImage.src = 'Assets/v1.png';
 }
 function init() {
     //define the objects that we will have in the game
@@ -12,21 +14,46 @@ function init() {
     //create a context
     pen = canvas.getContext('2d');
 
-    box = {
+    enemy1 = {
         x: 150,
         y: 50,
         w: 60,
         h: 60,
         speed: 20,
     };
+    enemy2 = {
+        x: 300,
+        y: 150,
+        w: 60,
+        h: 60,
+        speed: 30,
+    };
+    enemy3 = {
+        x: 450,
+        y: 20,
+        w: 60,
+        h: 60,
+        speed: 40,
+    };
+
+    enemies = [enemy1, enemy2, enemy3];
 }
 function draw() {
     //clear the canvas area for the old frame
     pen.clearRect(0, 0, W, H);
 
-    pen.fillRect(box.x, box.y, box.w, box.h);
+    enemies.forEach(enemy => {
+        pen.drawImage(enemyImage, enemy.x, enemy.y, enemy.w, enemy.h); 
+    });
 }
 function update() {
+    enemies.forEach(enemy => {
+        enemy.y += enemy.speed;
+        if(enemy.y > H - enemy.h || enemy.y < 0) {
+            enemy.speed *= -1;
+        }
+    })
+    
     //move the box downwards
     box.y += box.speed;
 
