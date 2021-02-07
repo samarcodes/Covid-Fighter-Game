27f01d4c0cc8b906dfe8f535f8b28d03a2e19d96
Samar Kalra <47415857+samarcodes@users.noreply.github.com>
2021-02-07 18:57:10 +0530

    basic setup for game

---
+++ game.js
@@ -0,0 +1,44 @@
+function loadImages() {
+    //player, virus, gem
+}
+function init() {
+    //define the objects that we will have in the game
+    canvas = document.getElementById('mycanvas');
+    W = 700;
+    H = 400;
+    canvas.width = W;
+    canvas.height = H;
+
+    //create a context
+    pen = canvas.getContext('2d');
+
+    box = {
+        x: 150,
+        y: 50,
+        w: 60,
+        h: 60,
+        speed: 20,
+    };
+}
+function draw() {
+    //clear the canvas area for the old frame
+    pen.clearRect(0, 0, W, H);
+
+    pen.fillRect(box.x, box.y, box.w, box.h);
+}
+function update() {
+    //move the box downwards
+    box.y += box.speed;
+
+    if(box.y > H - box.h || box.y < 0) {
+        box.speed *= -1;
+    }
+}
+function gameloop() {
+    draw();
+    update();
+}
+
+loadImages();
+init();
+var f = setInterval(gameloop, 100);
